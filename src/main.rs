47994c26b6ae210d8d38bb39997e938d70516737
Luke Wilkinson <98269518+TT1882@users.noreply.github.com>
2025-01-22 20:55:02 +0000

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -50,7 +50,7 @@ struct Options {
 
 
 fn main() {
-    println!("DO NOT USE. Fmp is getting rewritten and this version is unsafe!!")
+    println!("DO NOT USE. Fmp is getting rewritten and this version is unsafe!!");
     // Init
     // Arrays for password creation and entropy calculation
     let pass_Chars = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','1','2','3','4','5','6','7','8','9','0','!','"','#','$','%','&','\'','(',')','*','+',',','-','.','/',':',';','<','=','>','?','@','[','\\',']','^','_','`','{','|','}','~'];
