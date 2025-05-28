d8ecc60816b351ff3e9dfe712a5c42ddd6c99ec3
Luke <wilkinsonluke@proton.me>
2025-05-28 20:47:46 +0100

    Ensured compatibility with GPL v3.

--- src/flags.rs
+++ src/flags.rs
@@ -1,6 +1,24 @@
 //! This file preforms operations based on specified flags.
 //! These functions should only be called from the main function.
 
+/*
+Copyright (C) 2025  Luke Wilkinson
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+*/
+
 use crate::{
     crypto::securely_retrieve_password,
     input::{choice, input},
--- src/vault.rs
+++ src/vault.rs
@@ -1,6 +1,24 @@
 //! This file provides functionality for managing a vault.
 //! It includes functions for creating a vault, adding accounts, printing entries in the vault and more.
 
+/*
+Copyright (C) 2025  Luke Wilkinson
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+*/
+
 use crate::crypto::{decrypt_variable, encrypt_variable};
 use anyhow::Error;
 use dirs::data_dir;
